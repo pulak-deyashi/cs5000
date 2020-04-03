0206727a3f311248749bb087194f206ad11547bd
pulak-deyashi <54091088+pulak-deyashi@users.noreply.github.com>
2020-04-03 15:41:00 +0530

    comments added

--- recover/recover.c
+++ recover/recover.c
@@ -1,54 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<stdint.h>
-
-typedef uint8_t  BYTE;
-
+//FOR RAW DATA EXCLUDED SLAG SPACE  
 int main(int argc, char *argv[])
 {
-    if(argc != 2)
+    //check whether 2 arguments are passed in the command line
+    if (argc != 2)
     {
         printf("Usage: ./recover image\n");
         return 1;
     }
     int count = 0;
+    //file name of 8 chae long as 000.jpg
     char filename[8];
-    BYTE buffer[512];
+    //array of unsigned char ie 8 byte each space to store the read data initially
+    unsigned char buffer[512];
+    //new image file pointer
     FILE *img = NULL;
+    //opened the passed raw file
     FILE *file = fopen(argv[1], "r");
-    if(file == NULL)
+    //if passed file not exists print couldn't open and return a non zero value
+    if (file == NULL)
     {
         fprintf(stderr, "Could not open\n");
         return 2;
     }
-    while(fread(buffer, 1, 512, file) != 0x00)
+    //while loop upto the end of raw file
+    while (fread(buffer, 1, 512, file) != 0x00)
     {
-        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        //if jpeg image block found
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            if(count == 0)
+            //if the jpeg image block is not the first one then close the previously opened new img file
+            if (count != 0)
             {
+                fclose(img);
+            }
+            //create file name
             sprintf(filename, "%03i.jpg", count);
             count++;
+            //open a image file of that name to write the new image file
             img = fopen(filename, "w");
             fwrite(buffer, 1, 512, img);
-            }
-            else
-            {
-            fclose(img);
-            sprintf(filename, "%03i.jpg", count);
-            count++;
-            img = fopen(filename, "w");
-            fwrite(buffer, 1, 512, img);
-            }
         }
-        else if(count != 0)
+        //if any image file found before then add the block to the previous image file until new image starting block found
+        else if (count != 0)
         {
             fwrite(buffer, 1, 512, img);
         }
 
     }
-    if(img != NULL)
-    fclose(img);
+    //close the opened files
+    if (img != NULL)
+    {
+        fclose(img);
+    }
     fclose(file);
 
 }
