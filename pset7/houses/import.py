f298c86d12d9be270885b3af7182ab575fa0ae99
pulak-deyashi <pulakdeyashi@gmail.com>
2020-05-30 02:24:39 +0530

    files added

--- pset7/houses/import.py
+++ pset7/houses/import.py
@@ -1,1 +1,24 @@
 # TODO
+import csv, sqlite3
+from sys import argv
+def dataload(argv):
+    if len(argv) != 2:
+        print("input error")
+        return 1
+    db = sqlite3.connect('students.db')
+    with open(argv[1], 'r') as csvf:
+        dr = csv.DictReader(csvf)
+        for row in dr:
+            names = row['name'].split()
+            if len(names) == 2:
+                to_db = [names[0], None, names[1], row['house'], int(row['birth'])]
+                db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)", to_db)
+                db.commit()
+            elif len(names) == 3:
+                to_db = [names[0], names[1], names[2], row['house'], int(row['birth'])]
+                db.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)", to_db)
+                db.commit()
+    db.commit()
+    db.close()
+if(__name__ == "__main__"):
+    dataload(argv)
--- pset7/houses/roster.py
+++ pset7/houses/roster.py
@@ -1,1 +1,21 @@
 # TODO
+from sys import argv
+import sqlite3
+def printer(argv):
+    if len(argv) != 2:
+        print("Input error")
+        return 1
+
+    db = sqlite3.connect("students.db")
+    data = (db.execute(f"SELECT * FROM students WHERE house=\"{argv[1]}\" ORDER BY last, first")).fetchall()
+    db.close()
+    for row in data:
+        if row[2] == None:
+            print(f"{row[1]} {row[3]}, born {row[-1]}")
+        else:
+            print(f"{row[1]} {row[2]} {row[3]}, born {row[-1]}")
+
+
+
+if(__name__ == '__main__'):
+    printer(argv)
